eae2087f311b755ce6561c74b042d5188908c475
Crowe7 <jacobocrowe@gmail.com>
2021-12-11 15:20:55 -0600

    added tab js files and css file

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,26 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',  
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    contact: './src/contact.js',
+    menu: './src/contact.js',
+  },
   devtool: 'inline-source-map',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+      rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+      ],
   },
 };
