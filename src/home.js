f15c4f92e178faae04ee4f8f631d51dc30f3b19a
Crowe7 <jacobocrowe@gmail.com>
2021-12-12 01:58:06 -0600

    finished main page design

---
+++ src/home.js
@@ -0,0 +1,16 @@
+/* <body class="background">
+    <header class="header">
+        <h1>Generic Breakfast and Bad Coffee</h1>
+        <div class="button-wrapper">
+            <button id="homeTab" class="menu" type="button">Home</button>
+            <button id="menuTab" class="menu" type="button">Menu</button>
+            <button id="aboutTab" class="menu" type="button">About</button>
+        </div>
+    </header>
+    <div class="content-wrapper-wrapper">
+    <div class="content-wrapper">
+        <h2>This diner is really rundown and is used almost exclusivly for money laundering!</h2>
+        <p>Don't order here today!</p>
+    </div>
+    </div> */
+    
